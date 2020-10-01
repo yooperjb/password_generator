092c7853afb0065da991ec1eec66a46a3cb3925a
yooperjb <jmb181@humboldt.edu>
2020-10-01 16:58:23 -0700

    Added prompt windows

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,22 @@ function writePassword() {
 
 
 function generatePassword(){
+  // Prompt user for password length
   var passLength = window.prompt("What length password would you like to generate (Choose 8-128).");
-  
+  console.log(passLength);
+
+  // check if password is 8-128 characters
+  while (! (passLength >= 8 && passLength <= 128)) {
+    var passLength = window.prompt("Please choose a number from 8-128");
+  }
+
+  // prompt user if they want special characters
+  var lCase = window.confirm("Would you like to include lower case characters?");
+  var uCase = window.confirm("Would you like to include upper case characters?");
+  var numCase = window.confirm("Would you like to include numeric characters?");
+  var specCase = window.confirm("Would you like to include special characters?");
+
+
 };
 
 // Add event listener to generate button
