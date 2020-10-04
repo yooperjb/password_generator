c31d51350cc74b0a64506137a415a7e7c7a98c3d
Pedal <jmb181@humboldt.edu>
2020-10-04 15:54:24 -0700

    successful generator pre cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var letters = []
-var numbers = []
-var special = []
+//var letters = []
+//var numbers = []
+//var special = []
 
 // Write password to the #password input
 function writePassword() {
@@ -10,45 +10,37 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   // write to passwordText textarea in html page
-  //passwordText.value = password;
-  passwordText.value = "This is the where the Password Will GO";
-
+  passwordText.value = password;
 };
 
-
 function generatePassword() {
-
+  
+  pw = '';
   // Prompt user for password length
   var passLength = window.prompt("What length password would you like to generate (Choose 8-128).");
  
-
   // check if password is 8-128 characters
   while (!(passLength >= 8 && passLength <= 128)) {
     var passLength = window.prompt("Please choose a number from 8-128");
   }
   
-  console.log(passLength);
-  // run passOptions to get character options
-  passOptions();
-
-/*
-  for (i=0, i < passLength, i++) {
-
+  // run passOptions to get character options - returns array of true choices
+  var trueKeysArr = passOptions();
+  
+  // create random chars for the length of the password chosen
+  for (i = 0; i < passLength; i++) {
+    
+    // get a random char type for those selected
+    charType = trueKeysArr[Math.floor(Math.random()* trueKeysArr.length)];
+    // add random char to pw array calling generateChar
+    pw = pw + (generateChar(charType));
+    
   }
-*/
+  return pw;
+
 };
 
-/*
-String.fromCharCode(57);
-This would be 9. This is from the UTF-16 table
-
-Nums are from CharCode 48-57
-Capital letters from 65-90
-Lower case letters from 97-122
-Special characters are numerous so maybe best to just put in array?
-
-*/
-
+// user selected password charcter options. Returns array of true choices.
 function passOptions() {
   var options = {
     lCase: false,
@@ -69,41 +61,47 @@ function passOptions() {
   // prompt user if they want special characters
   options.specCase = window.confirm("Would you like to include special characters?");
 
-  // check if every option is false, if so alert user
+  // check if every option is false, if so alert user and ask again
   if (Object.values(options).every(item => !item)) {
     window.alert("You must select at least one type of character type.");
-    console.log(options);
     passOptions();
   }
   else {
-    console.log("Sweet, at least one option was selected");
-    console.log(options);
+    // get list of passOptions keys
     var optionKeys = Object.keys(options);
+    // create an array of true keys
     var trueKeys = optionKeys.filter(function(id) {
-    return options[id];
+      return options[id];
     })
-    
     return trueKeys;
-    //return options;
   }
-
 };
 
-// should switch be used here?
+// generate a random character based on char type
 function generateChar(char) {
-  if (char === 'lCase') {
+  
+  switch(char) {
 
+    case 'lCase': // generate random lower case character
+      tempChar = String.fromCharCode(Math.floor((Math.random() * 26)+ 97));
+      console.log("Random lower case is: " + tempChar);
+      return tempChar;
+
+    case 'uCase': // generate random upper case character
+      tempChar = String.fromCharCode(Math.floor((Math.random() * 26)+ 65));
+      console.log("Random upper case is: " + tempChar);
+      return tempChar;
+
+    case 'numCase': // generate random number
+      tempChar = String.fromCharCode(Math.floor((Math.random() * 10)+ 48));
+      console.log("Random Number is: " + tempChar);
+      return tempChar;
+    
+    case 'specCase': // generate random spec character
+      tempChar = String.fromCharCode(Math.floor((Math.random() * 15)+ 33));
+      console.log("Random Special Char is: " + tempChar);
+      return tempChar; 
   }
-  else (char === 'uCase') {
-
-  }
-  else (char === 'numCase') {
-
-  }
-  else (char === 'pecCase') {
-
-  }
-
 };
 
 // Add event listener to generate button
